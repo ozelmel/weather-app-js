f114e44cffc8da9af96f0564c6d9eb105ed7f382
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-23 14:26:10 +0300

    project structure and encrypt files added

---
+++ weather.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("section.top-banner form")
+const input = document.querySelector(".top-banner input")
+const msg = document.querySelector("span.msg")
+const list = document.querySelector(".ajax-section .cities")
+
+//apiKey=fda926dd8473bdc8852b2bc56a4cf2c1
+
+localStorage.setItem("apiKey", EncryptStringAES("fda926dd8473bdc8852b2bc56a4cf2c1"));
+
+
